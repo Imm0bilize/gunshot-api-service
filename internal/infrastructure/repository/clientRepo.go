bc6ca556fe07e626fa3f69e72ead31bb9eaf6503
Nikolay Vedernikov <nick.vedernikow@yandex.ru>
2022-10-29 18:34:35 +0300

    Added tracing in client repo

--- internal/infrastructure/repository/clientRepo.go
+++ internal/infrastructure/repository/clientRepo.go
@@ -4,15 +4,23 @@ import (
 	"context"
 	"fmt"
 	"github.com/go-redis/redis/v9"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
 )
 
 type ClientRepo struct {
-	db *redis.Client
+	db     *redis.Client
+	tracer trace.Tracer
 }
 
 // Create save a new user with uuid and information about him
 func (c *ClientRepo) Create(ctx context.Context, uid string, info []byte) error {
+	ctx, span := c.tracer.Start(ctx, "ClientRepo.Create")
+	defer span.End()
+
 	if err := c.db.Set(ctx, uid, info, 0).Err(); err != nil {
+		span.SetStatus(codes.Error, fmt.Sprintf("failed to write the client to the database: %s", err.Error()))
 		return fmt.Errorf("failed to write the client to the database: %w", err)
 	}
 	return nil
@@ -20,8 +28,12 @@ func (c *ClientRepo) Create(ctx context.Context, uid string, info []byte) error 
 
 // Delete remove the user from database
 func (c *ClientRepo) Delete(ctx context.Context, uid string) error {
+	ctx, span := c.tracer.Start(ctx, "ClientRepo.Delete")
+	defer span.End()
+
 	n, err := c.db.Del(ctx, uid).Result()
 	if err != nil {
+		span.SetStatus(codes.Error, err.Error())
 		return fmt.Errorf("failed to delete the user from the database: %w", err)
 	}
 
@@ -33,7 +45,9 @@ func (c *ClientRepo) Delete(ctx context.Context, uid string) error {
 }
 
 func NewClientRepo(db *redis.Client) *ClientRepo {
+	tracer := otel.Tracer("ClientRepo")
 	return &ClientRepo{
-		db: db,
+		db:     db,
+		tracer: tracer,
 	}
 }
