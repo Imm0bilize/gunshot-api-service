08e9bc6158ee0a8031145b50b80d6275e5233d0a
Nikolay Vedernikov <nick.vedernikow@yandex.ru>
2022-10-29 18:37:59 +0300

    Added base implem of handlers

--- internal/controller/http/v1/handlers.go
+++ internal/controller/http/v1/handlers.go
@@ -1,7 +1,12 @@
 package v1
 
 import (
+	"github.com/Imm0bilize/gunshot-api-service/internal/controller/http/dto"
+	"github.com/Imm0bilize/gunshot-api-service/internal/infrastructure/repository"
+	"github.com/Imm0bilize/gunshot-api-service/internal/uCase"
 	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
 	"net/http"
 )
 
@@ -10,9 +15,57 @@ func (h *Handler) UploadAudio(c *gin.Context) {
 }
 
 func (h *Handler) RegisterNewClient(c *gin.Context) {
+	var clientRequest dto.NewClientRequest
+
+	if err := c.ShouldBindJSON(&clientRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	h.logger.Info(
+		"got new request for creating new clientRequest",
+		zap.String("reqID", clientRequest.RequestID.String()),
+	)
+
+	id, err := h.domain.CreateNewClient(
+		c.Request.Context(), clientRequest.RequestID.String(), clientRequest.ClientInformation,
+	)
+
+	if err != nil {
+		if errors.Is(err, uCase.ErrRequestAlreadyProcessed) {
+			c.JSON(http.StatusConflict, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"clientID": id})
+	return
+}
+
+func (h *Handler) UpdateClient(c *gin.Context) {
 	c.AbortWithStatus(http.StatusNotImplemented)
 }
 
 func (h *Handler) DeleteClient(c *gin.Context) {
-	c.AbortWithStatus(http.StatusNotImplemented)
+	var request dto.DeleteClientRequest
+
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	err := h.domain.DeleteClient(c.Request.Context(), request.RequestID.String(), request.ClientID.String())
+	if err != nil {
+		if errors.As(err, &repository.ErrClientNotFound) {
+			c.JSON(http.StatusNotFound, gin.H{"error": "the client not found"})
+			return
+		}
+
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.String(http.StatusOK, "ok")
 }
