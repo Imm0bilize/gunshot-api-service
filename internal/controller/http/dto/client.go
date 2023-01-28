aa40529086ccf995913cc591c95fec38b51cdf45
Nikolay Vedernikov <nick.vedernikow@yandex.ru>
2023-01-28 16:03:20 +0300

    Update dto messages

--- internal/controller/http/dto/client.go
+++ internal/controller/http/dto/client.go
@@ -1,44 +1,16 @@
 package dto
 
-import (
-	"github.com/google/uuid"
-	"time"
-)
-
-type NewClientRequest struct {
-	RequestID         uuid.UUID  `json:"request_id"`
-	ClientInformation ClientInfo `json:"client_info"`
-}
-
-type DeleteClientRequest struct {
-	RequestID uuid.UUID `json:"request_id"`
-	ClientID  uuid.UUID `json:"client_id"`
-}
-
-type UpdateClientRequest struct {
-	RequestID      uuid.UUID  `json:"request_id"`
-	ClientID       uuid.UUID  `json:"client_id"`
-	NewInformation ClientInfo `json:"client_info"`
-}
-
-type AudioJsonRequest struct {
-	RequestID uuid.UUID `json:"request_id"`
-	ClientID  uuid.UUID `json:"client_id"`
-	Timestamp time.Time `json:"timestamp"`
-	Payload   string    `json:"payload"`
-}
-
-type AudioProtoRequest struct {
-	RequestID uuid.UUID `json:"request_id"`
-	ClientID  uuid.UUID `json:"client_id"`
-	Timestamp time.Time `json:"timestamp"`
-	Payload   []byte    `json:"payload"`
-}
+import "time"
 
 type ClientInfo struct {
-	FullName            string   `json:"full_name"`
-	LocationName        string   `json:"location_name"`
-	Latitude            string   `json:"latitude"`
-	Longitude           string   `json:"longitude"`
-	NotificationMethods []string `json:"notification_methods"`
+	LocationName        string   `json:"locationName" binding:"required"`
+	FullName            string   `json:"fullName" binding:"required"`
+	Latitude            float64  `json:"latitude" binding:"required"`
+	Longitude           float64  `json:"longitude" binding:"required"`
+	NotificationMethods []string `json:"notificationMethods" binding:"required"`
+}
+
+type UploadAudioRequest struct {
+	Timestamp time.Time `url:"ts" binding:"required"`
+	ID        string    `url:"id" binding:"required"`
 }
